3e90a99e073d8b46e148b9bdebc7bbaeb9ea986e
CodingWinner <130879463+CodingWinner@users.noreply.github.com>
2024-12-09 21:48:27 -0500

    Update cl_initializer_base.h
    Made the devices and program available to the user.

--- source/cl_initializer_base.h
+++ source/cl_initializer_base.h
@@ -15,8 +15,10 @@
 #define CL_TARGET_OPENCL_VERSION 300
 #include <CL/cl.h>
 
+extern cl_device_id *devices;
 extern cl_context context;
 extern cl_command_queue queue;
+extern cl_program program;
 extern cl_kernel *kernels;
 
 #endif
